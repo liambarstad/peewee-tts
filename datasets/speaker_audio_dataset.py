0ae57e401603066ca77157d94398278aba1b42af
liambarstad <lbarstad@aligntechandfinance.com>
2023-02-15 16:32:24 -0800

    encoder

--- datasets/speaker_audio_dataset.py
+++ datasets/speaker_audio_dataset.py
@@ -33,6 +33,30 @@ class SpeakerAudioDataset(Dataset):
         if 'LibriTTS' in repos:
             self._load_libritts(repos['LibriTTS'])
         
+    def _load_libritts(self, info={}):
+        # loads all utterances into data [ dataframe, speaker, path ]
+        file_paths = self.source.member_paths('/LibriTTS/'+info['version'])
+        data = []
+        for path in file_paths:
+            if path[-3:] == 'wav':
+                data.append([
+                    'LibriTTS', 
+                    path.split('/')[-1].split('_')[0], 
+                    path
+                ])
+        self._add_to_paths(data)
+
+    def _add_to_paths(self, data):
+        # loads data into dataframe -- dataset | speaker | path | speaker_id
+        existing_data = self.paths[['dataset', 'speaker', 'path']]
+        data = pd.DataFrame(data, columns=existing_data.columns)
+        new_data = pd.concat([existing_data, data]).drop_duplicates()
+        # add unique speaker_id column
+        unique = new_data[['dataset', 'speaker']].drop_duplicates().reset_index().reset_index()
+        unique = unique.rename(columns={unique.columns[0]:'speaker_id'})[['dataset', 'speaker', 'speaker_id']]
+        self.paths = pd.merge(new_data, unique, on=['dataset', 'speaker'], how='left')
+        return len(self.paths)
+
     def __len__(self):
         return self.paths.speaker_id.max() + 1
 
@@ -53,30 +77,5 @@ class SpeakerAudioDataset(Dataset):
         values = self.transform(speaker_data)
         labels = np.array([ idx for _ in range(self.m_utterances) ])
         return labels, values
-
-
-    def _load_libritts(self, info={}):
-        # loads all utterance into dataset | speaker | path | speaker_id
-        file_paths = self.source.member_paths('/LibriTTS/'+info['version'])
-        data = []
-        for path in file_paths:
-            if path[-3:] == 'wav':
-                data.append([
-                    'LibriTTS', 
-                    path.split('/')[-1].split('_')[0], 
-                    path
-                ])
-        self._add_to_paths(data)
-
-    def _add_to_paths(self, data):
-        existing_data = self.paths[['dataset', 'speaker', 'path']]
-        data = pd.DataFrame(data, columns=existing_data.columns)
-        new_data = pd.concat([existing_data, data]).drop_duplicates()
-        # add unique speaker_id column
-        unique = new_data[['dataset', 'speaker']].drop_duplicates().reset_index().reset_index()
-        unique = unique.rename(columns={unique.columns[0]:'speaker_id'})[['dataset', 'speaker', 'speaker_id']]
-        self.paths = pd.merge(new_data, unique, on=['dataset', 'speaker'], how='left')
-
-        return len(self.paths)
 
 
